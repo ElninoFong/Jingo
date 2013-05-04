e29ac559d43b32da3324fff7dc8d6a47aa33c322
ElninoFong <ElninoFong@me.com>
2013-05-04 01:25:35 -0400

    schedule done

--- notes.py
+++ notes.py
@@ -6,12 +6,29 @@ SECRET_KEY = 'development key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-user = {'uid': 2,
+user = {'uid': 1,
 		'username': 'James',
 		'email': 'James@gmail.com',
 		'profile_id': 6,
 		'last_loc_name': 'soho',
-		'state_id': 2}
+		'state_id': 2,
+		'state_name': 'at work'}
+
+state = {'at work',
+		 'lunch break',
+		 'just chilling'}
+
+dayofweek = [{'day': 'Monday', 'id': 0},
+			 {'day': 'Tuesday', 'id': 1},
+			 {'day': 'Wednesday', 'id': 2},
+			 {'day': 'Thursday', 'id': 3},
+			 {'day': 'Friday', 'id': 4},
+			 {'day': 'Saturday', 'id': 5},
+			 {'day': 'Sunday', 'id': 6}]
+
+repeat = [{'type': 'Never', 'id': 0},
+		  {'type': 'Every Day', 'id': 1},
+		  {'type': 'Every Week', 'id': 2}]
 
 def connect_db():
 	return mdb.connect('127.0.0.1', 'root', 'root', 
@@ -46,15 +63,18 @@ def write_notes():
 	if request.method == 'POST':
 		if request.form['words']:
 			cur = g.db.cursor()
-			query_add = "INSERT INTO NOTE (words) VALUES (%s)"
-			cur.execute(query_add, request.form['words'])
+			query_add = "INSERT INTO NOTE (uid, words) VALUES (%s, %s)"
+			cur.execute(query_add, (user['uid'], request.form['words']))
 			g.db.commit()
 			# flash(request.form['words'])
 			flash("Write a new note.")
 			return redirect(url_for("show_all_notes"))
 		else:
 			flash("Invalid input.")
-	return render_template('write_notes.html')
+	return render_template('write_notes.html',
+		user = user,
+		dayofweek = dayofweek,
+		repeat = repeat)
 
 @app.route('/recieve_notes')
 def recieve_notes():
@@ -87,3 +107,27 @@ def my_notes():
 		table = 'NOTES',
 		fields = fields,
 		results = results)
+
+@app.route('/filter', methods=['GET', 'POST'])
+def filter():
+	if request.method == 'POST':
+		if request.form['newstate'] != '':
+			if request.form['newstate'] not in state:
+				state.append(request.form['newstate'])
+				user['state_name'] = request.form['newstate']
+				flash("Add a New State")
+			else: 
+				user['state_name'] = request.form['newstate']
+				flash("State Changed")
+		elif request.form['selstate'] != '':
+			user['state_name'] = request.form['selstate']
+			flash("State Changed")
+		else:
+			flash("Nothing Change")
+	return render_template('filter.html',
+		user = user,
+		state = state)
+
+@app.route('/test')
+def test():
+	return render_template('test.html')
