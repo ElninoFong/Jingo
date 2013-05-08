0407e83395dfb73ab35c76fc91f2c4a48268e8ed
ElninoFong <ElninoFong@me.com>
2013-05-08 04:50:05 -0400

    add filter

--- dbprocess.py
+++ dbprocess.py
@@ -5,32 +5,19 @@ from config import tags
 
 class dbprocess():
 	def insert_note(self, uid=None, words='', link=None, loc_id=None,
-					radius=None, repeat=0, startdatetime=None,
-					enddatetime=None, starttime=None, endtime=None,
-					dow=None, selecttag=[], addtag=[]):
+					radius=None, schedule_id=None, selecttag=[], addtag=[]):
 		cur = g.db.cursor()
-		# add schedule
-		if (repeat == '0'):	# Never repeat
-			start = str(startdatetime) + ':00'
-			end = str(enddatetime) + ':00'
-		else:	# Every Day or Every Week
-			start = str(date.today()) + ' ' + str(starttime) + ':00'
-			end = str(date.today()) + ' ' + str(endtime) + ':00'
-		query_add_schedule = "INSERT INTO SCHEDULE (starttime, endtime, repeat_id, dayofweek) \
-					VALUES (%s, %s, %s ,%s)"
-		cur.execute(query_add_schedule, (start, end, repeat, dow))
-		schedule_id = g.db.insert_id()
-		flash("Add new schedule: " + str(schedule_id))
-
 		# add tag
 		tag_id = []
-		for a in addtag:	# remove same tags
-			if a in selecttag:
-				addtag.remove(a)
-		for s in selecttag:	# predefined tag id
-			for row in tags:
-				if s in row['name']:
-					tag_id.append(row['id'])
+		if addtag and addtag[0] is not u'':
+			for a in addtag:	# remove same tags
+				if a in selecttag:
+					addtag.remove(a)
+		if selecttag and selecttag[0] is not u'':
+			for s in selecttag:	# predefined tag id
+				for row in tags:
+					if s in row['name']:
+						tag_id.append(row['id'])
 		if addtag and addtag[0] is not u'': 	# use this condition for unicode empty list
 			query_show_tag = "SELECT * FROM TAG"
 			cur.execute(query_show_tag)
@@ -48,8 +35,7 @@ class dbprocess():
 					tag_id.append(g.db.insert_id())
 
 		# add note
-		query_add_note = "INSERT INTO NOTE (uid, words, hyperlink, location_id, radius, schedule_id) \
-				VALUES (%s, %s, %s, %s, %s, %s)"
+		query_add_note = "INSERT INTO NOTE (uid, words, hyperlink, location_id, radius, schedule_id) VALUES (%s, %s, %s, %s, %s, %s)"
 		cur.execute(query_add_note, (uid, words, link, loc_id, radius, schedule_id))
 		note_id = g.db.insert_id()
 		flash("Write a new note: " + str(note_id))
@@ -62,6 +48,56 @@ class dbprocess():
 		g.db.commit()
 		# flash("Add tags_in_note.")
 
+	def add_filter(self, state_id=None, schedule_id=None, loc_id=None, 
+					filter_radius=None, selecttag=[], addtag=[]):
+		cur = g.db.cursor()
+		# add tag
+		tag_id = []
+		if addtag and addtag[0] is not u'':
+			for a in addtag:	# remove same tags
+				if a in selecttag:
+					addtag.remove(a)
+		if selecttag and selecttag[0] is not u'':
+			for s in selecttag:	# predefined tag id
+				for row in tags:
+					if s in row['name']:
+						tag_id.append(row['id'])
+		if addtag and addtag[0] is not u'': 	# use this condition for unicode empty list
+			query_show_tag = "SELECT * FROM TAG"
+			cur.execute(query_show_tag)
+			results = cur.fetchall()
+			for a in addtag:
+				exist = False
+				for i,n in results:
+					if a == n:	# exist
+						tag_id.append(i)
+						exist = True
+						break
+				if not exist:	# new
+					query_add_tag = "INSERT INTO TAG (tag_name) VALUES (%s)"
+					cur.execute(query_add_tag, (a))
+					tag_id.append(g.db.insert_id())
+		print "selecttag: " 
+		print selecttag
+		print "addtag: "
+		print addtag
+		print "tag_id[]: "
+		print tag_id
+
+		# add filter
+		query_add_filter = "INSERT INTO FILTER (state_id, location_id, filter_radius, schedule_id) VALUES (%s, %s, %s, %s)"
+		cur.execute(query_add_filter, (state_id, loc_id, filter_radius, schedule_id))
+		filter_id = g.db.insert_id()
+		flash("Add a new filter: " + str(filter_id))
+
+		# add tags_in_filter
+		query_add_tags_in_filter = "INSERT INTO TAGS_IN_FILTER (filter_id, tag_id) VALUES (%s, %s)"
+		for t in tag_id:
+			cur.execute(query_add_tags_in_filter, (filter_id, t))
+		g.db.commit()
+		flash("Succeeded to add tags_in_filter.")
+
+
 	def get_location_id(self, loc=None):
 		# add location to db and get location id
 		results = loc.split(';')
@@ -72,10 +108,42 @@ class dbprocess():
 		query = "INSERT INTO `Jingo_DB`.`LOCATION` (`location_id`, `latitude`, `longitude`, `location_name`) VALUES (NULL, %s, %s, %s);"
 		cur.execute(query, (results[1],results[2],results[0]))
 		loc_id = g.db.insert_id()
-		flash("Add location " + results[0] + ": " + str(loc_id))
+		flash("Add location: " + results[0] + ": " + str(loc_id))
 		return loc_id
 
-# if __name__ == '__main__':
-	# db = mdb.connect('127.0.0.1', 'root', 'root', 
-	# 	'Jingo_DB', port=8889);
-	# cur = db.cursor()
+	def get_schedule_id(self, repeat='0', startdatetime=None, enddatetime=None, 
+						starttime=None, endtime=None, dow=None,):
+		# add schedule to db and get schedule id
+		if (repeat == '0'):	# Never repeat
+			start = str(startdatetime) + ':00'
+			end = str(enddatetime) + ':00'
+		else:	# Every Day or Every Week
+			start = str(date.today()) + ' ' + str(starttime) + ':00'
+			end = str(date.today()) + ' ' + str(endtime) + ':00'
+		cur = g.db.cursor()
+		query_add_schedule = "INSERT INTO SCHEDULE (starttime, endtime, repeat_id, dayofweek) VALUES (%s, %s, %s ,%s)"
+		cur.execute(query_add_schedule, (start, end, repeat, dow))
+		schedule_id = g.db.insert_id()
+		flash("Add new schedule: " + str(schedule_id))
+		return schedule_id
+
+	def add_state(self, uid, newstate):
+		cur = g.db.cursor()
+		query_add_state = "INSERT INTO STATE (uid, state_name) VALUES (%s ,%s)"
+		cur.execute(query_add_state, (uid, newstate))
+		state_id = g.db.insert_id()
+		g.db.commit()
+		flash("Add new state: " + newstate + ", id: " + str(state_id))
+		return state_id
+
+if __name__ == '__main__':
+	db = mdb.connect('127.0.0.1', 'root', 'root', 
+		'Jingo_DB', port=8889);
+	cur = db.cursor()
+	cur.execute("SELECT state_id FROM STATE WHERE uid = %s AND state_name = %s", (1, 'lunch break'))
+	# results = [row[0] for row in cur.fetchall()]
+	# if cur.fetchone():
+	# 	result = cur.fetchone()[0]
+	# else:
+	# 	result = None
+	# print result
--- views.py
+++ views.py
@@ -17,8 +17,7 @@ user = {'uid': 1,
 		'state_name': 'at work'}
 
 def connect_db():
-	return mdb.connect('127.0.0.1', 'root', 'root', 
-		'Jingo_DB', port=8889);
+	return mdb.connect('127.0.0.1', 'root', 'root', 'Jingo_DB', port=8889)
 
 @app.before_request
 def before_request():
@@ -51,7 +50,7 @@ def write_notes():
 					'enddatetime': '',
 					'starttime': '',
 					'endtime': '',
-					'radius': 1,
+					'radius': 500,
 					'selecttag': '',
 					'addtag': ''}
 	if request.method == 'POST':
@@ -70,11 +69,11 @@ def write_notes():
 			selecttag = request.form['jquery-tagbox-select'].split(',')
 			addtag = request.form['jquery-tagbox-text'].split(',')
 			loc_id = process.get_location_id(loc=request.form['loc'])
-			process.insert_note(uid=user['uid'], words=request.form['words'], link=request.form['link'],
-								loc_id=loc_id, radius=request.form['radius'], repeat=request.form['repeat_sel'],
-								startdatetime=request.form['startdatetime'], enddatetime=request.form['enddatetime'],
-								starttime=request.form['starttime'], endtime=request.form['endtime'],
-								dow=request.form['dow_sel'], selecttag=selecttag, addtag=addtag)
+			schedule_id = process.get_schedule_id(repeat=request.form['repeat_sel'], startdatetime=request.form['startdatetime'],
+												  enddatetime=request.form['enddatetime'], starttime=request.form['starttime'], 
+												  endtime=request.form['endtime'], dow=request.form['dow_sel'])
+			process.insert_note(uid=user['uid'], words=request.form['words'], link=request.form['link'], loc_id=loc_id, 
+								radius=request.form['radius'], schedule_id=schedule_id, selecttag=selecttag, addtag=addtag)
 			return redirect(url_for("show_all_notes"))
 	return render_template('write_notes.html',
 		user = user,
@@ -106,7 +105,7 @@ def my_notes():
 	query_my = "SELECT * FROM NOTE WHERE uid = %s"
 	cur.execute(query_field)
 	fields = [row[0] for row in cur.fetchall()]
-	cur.execute(query_my, (user['uid']))
+	cur.execute(query_my, (user['uid'],))
 	results = cur.fetchall()
 	flash(user['username'] + " published following notes.")
 	return render_template('show_notes.html',
@@ -117,23 +116,62 @@ def my_notes():
 
 @app.route('/filter', methods=['GET', 'POST'])
 def filter():
+	cur = g.db.cursor()
 	if request.method == 'POST':
-		if request.form['newstate']:
-			if request.form['newstate'] not in state:
-				state.append(request.form['newstate'])
-				user['state_name'] = request.form['newstate']
-				flash("Add a New State")
-			else: 
-				user['state_name'] = request.form['newstate']
-				flash("State Changed")
-		elif request.form['selstate']:
-			user['state_name'] = request.form['selstate']
-			flash("State Changed")
-		else:
-			flash("Nothing Change")
+		valid = True
+		state_id = None
+		if not request.form['newstate'] and not request.form['state_sel']:
+			flash("State is required.")
+			valid = False
+		elif request.form['state_sel']:
+			cur.execute("SELECT state_id FROM STATE WHERE uid = %s AND state_name = %s", (user['uid'], request.form['state_sel']))
+			results = cur.fetchone()
+			if results is not None:
+				state_id = results[0]
+			else:
+				state_id = process.add_state(user['uid'], request.form['state_sel'])	# add state to db
+			user['state_name'] = request.form['state_sel']
+		elif request.form['newstate']:
+			cur.execute("SELECT state_id FROM STATE WHERE uid = %s AND state_name = %s", (user['uid'], request.form['newstate']))
+			results = cur.fetchone()
+			if results is not None:
+				state_id = results[0]
+			else:
+				state_id = process.add_state(user['uid'], request.form['newstate'])	# add state to db
+			user['state_name'] = request.form['newstate']
+
+		if not ((request.form['startdatetime'] and request.form['enddatetime']) or (request.form['starttime'] and request.form['endtime'])):
+			valid = False
+			flash("Schedule is required.")
+
+		if valid:
+			# add schedule to db
+			schedule_id = process.get_schedule_id(repeat=request.form['repeat_sel'], startdatetime=request.form['startdatetime'],
+												  enddatetime=request.form['enddatetime'], starttime=request.form['starttime'], 
+												  endtime=request.form['endtime'], dow=request.form['dow_sel'])
+			if request.form['loc']:
+				loc_id = process.get_location_id(loc=request.form['loc'])
+			else:
+				loc_id = None
+			selecttag = request.form['jquery-tagbox-select'].split(',')
+			addtag = request.form['jquery-tagbox-text'].split(',')
+			# add a new filter
+			process.add_filter(state_id=state_id, schedule_id=schedule_id, loc_id=loc_id, filter_radius=request.form['filter_radius'],
+								selecttag=selecttag, addtag=addtag)
+	fields = ['uid', 'username', 'state_id', 'state_name', 'filter_id', 'filter_radius', 'schedule_id', 'starttime', 'endtime', 'dow_name', 'location_id', 'location_name' 'tag_name']
+	query_filter = "SELECT uid, username, state_id, state_name, filter_id, filter_radius, schedule_id, starttime, endtime, dow_name, location_id, location_name, tag_name \
+		FROM USER NATURAL JOIN STATE NATURAL JOIN FILTER NATURAL JOIN SCHEDULE NATURAL LEFT JOIN LOCATION NATURAL JOIN DAYOFWEEK NATURAL LEFT JOIN TAGS_IN_FILTER NATURAL LEFT JOIN TAG \
+		WHERE dayofweek = dow_id AND uid = %s"
+	cur.execute(query_filter, (user['uid'],))
+	results = cur.fetchall()
 	return render_template('filter.html',
 		user = user,
-		state = state)
+		dayofweek = dayofweek,
+		repeat = repeat,
+		tags = tags,
+		state = state,
+		results = results,
+		fields = fields)
 
 @app.route('/map')
 def map():
